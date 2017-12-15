11e94bf3718012f4ab5cf5c37278efe690b3e9ff
mcvinding <mikkel.vinding@gmail.com>
2017-12-15 14:36:59 +0100

    First commit. Added some files.

---
+++ avgHeadPos.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Wrapper for creating average headposition files that can be passed to maxfilter.
+
+helptxt='Use as: .../avgHeadPos [name of conditions to combine*]
+*consistent naming of files is required. The script will combine all files in current dir starting with that name.
+Multiple arguments can be passed. Each name is processed seperatly'
+
+if [ -z "$@" ]; then
+	echo "$helptxt"
+	return
+fi
+
+path_to_pyfile='/home/natmeg/data_scripts/avg_headpos' #Change depending on which computer is used!
+
+for CON in "$@"
+do
+	echo "$CON"
+	python $path_to_pyfile/avgHEadPos.py $CON
+done
+
